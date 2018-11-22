8d553d886629c28801e4ab5161d277b72f1765bc
FlorenceSoudain <43751399+FlorenceSoudain@users.noreply.github.com>
2018-11-22 21:41:42 +0100

    V1

--- script.js
+++ script.js
@@ -9,47 +9,55 @@ var poubelleM = document.getElementById('marron');
 var bouteillesPlastiques = new Image();
 bouteillesPlastiques.src = 'images/bouteilles-plastiques.jpg';
 dechets.push(bouteillesPlastiques);
-bouteillesPlastiques.className = "dechets";
+bouteillesPlastiques.className = "jaune";
 bouteillesPlastiques.id = "bouteillePlastiques";
-document.getElementById('dechets').appendChild(bouteillesPlastiques);
 bouteillesPlastiques.alt = "Bouteilles en plastiques";
 
 var bouteillesLait = new Image();
 bouteillesLait.src = 'images/bouteilles-lait.jpg';
 dechets.push(bouteillesLait);
-bouteillesLait.className = "dechets";
-document.getElementById('dechets').appendChild(bouteillesLait);
+bouteillesLait.className = "jaune";
 bouteillesLait.alt = "Bouteille de lait";
 
 var bouteillesVerres = new Image();
 bouteillesVerres.src = 'images/bouteilles-verres.jpg';
 dechets.push(bouteillesVerres);
-bouteillesVerres.className = "dechets";
-document.getElementById('dechets').appendChild(bouteillesVerres);
+bouteillesVerres.className = "vert";
 bouteillesVerres.alt = "Bouteilles en Verres";
 
 var canette = new Image();
 canette.src = 'images/canette.jpg';
 dechets.push(canette);
-canette.className = "dechets";
-document.getElementById('dechets').appendChild(canette);
+canette.className = "jaune";
 canette.alt = "Canette";
 
 var magazines = new Image();
 magazines.src = 'images/magazines.jpg';
 dechets.push(magazines);
-magazines.className = "dechets";
-document.getElementById('dechets').appendChild(magazines);
+magazines.className = "bleu";
 magazines.alt = "Une pile de magazines";
 
 var journaux = new Image();
 journaux.src = 'images/journaux.jpg';
 dechets.push(journaux);
-journaux.className = "dechets";
-document.getElementById('dechets').appendChild(journaux);
+journaux.className = "bleu";
 journaux.alt = "Une pile de journaux";
 
+document.getElementById("jouer").addEventListener("click", function (){
 
+    displayImage();
+});
+
+document.getElementById('jaune').addEventListener('click', function () {
+    if(random == document.getElementsByClassName('jaune')){
+        alert('bravo');
+    }
+});
+function displayImage() {
+    var num = Math.floor(Math.random() * (dechets.length+1));
+    document.getElementById("dechet").innerHTML = '<img'+ dechets[num];
+
+}
 
 console.log(dechets.length);
 for (i = 0; i < dechets.length; i++) {
